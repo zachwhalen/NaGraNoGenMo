d9486983a7553b53f9a7ff2be997073da88bde6c
Zach Whalen <zach.whalen@gmail.com>
2014-11-15 11:48:31 -0500

    just some minor path cleanup

--- img/montage.pl
+++ img/montage.pl
@@ -1,13 +1,14 @@
 #!/usr/bin/local/perl
 
 use File::Basename;
-
+my $base = '/home/zach/Software/pg';
+my $path = '/nagranogenmo/NaGraNoGenMo';
 # create a 3x3 grid of panels
 
-@files = glob "/home/zach/Software/pg/tube/candidates/*.jpg";
+@files = glob "$base/tube/candidates/*.jpg";
 
 
-@panels = glob "/home/zach/Software/pg/fifty/img/tmp/*.jpg";
+@panels = glob "$base/$path/img/tmp/*.jpg";
 foreach (@panels){
 	unlink;
 }
@@ -17,14 +18,14 @@ for ($t = 0; $t <9; $t++){
 	$file = $files[int(rand($#files))];
 
 	$fn = basename $file;
-	system("convert $file -resize \"1000x\" -crop 200x300+20+20 tmp/$fn");
+	system("convert $file -resize \"1000x\" -crop 200x300+20+20 $base/$path/img/tmp/$fn");
 
 }
 
-@panels = glob "/home/zach/Software/pg/fifty/img/tmp/*.jpg";
+@panels = glob "$base/$path/img/tmp/*.jpg";
 
 $pan = join (" ", @panels);
 
-system("montage $pan -tile 3x -geometry +4+4 -border 2 -bordercolor #202020 /home/zach/Software/pg/fifty/img/page.jpg");
+system("montage $pan -tile 3x -geometry +4+4 -border 2.5 -bordercolor #000000 $base/$path/img/page.jpg");
 
-system("convert /home/zach/Software/pg/fifty/img/page.jpg -bordercolor \"#ffffff\" -border 12  /home/zach/Software/pg/fifty/img/page.jpg")
+system("convert $base/$path/img/page.jpg -bordercolor \"#ffffff\" -border 12  $base/$path/img/page.jpg")
