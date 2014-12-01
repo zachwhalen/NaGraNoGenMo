e1ad7f21ed7371c5d4cbaef80665b04a0bfb50ce
Zach Whalen <zach.whalen@gmail.com>
2014-12-01 08:19:29 -0500

    Look harder for text sources

--- granogen.pl
+++ granogen.pl
@@ -553,68 +553,74 @@ sub getLegs {
 	my @tweets;
 	my @legs;
 
-	%params = (
-		'q' => '%23tbt+when',
-		'apikey' => '09C43A9B270A470B8EB8F2946A9369F3', # I don't know how long this one will work, but should be able to switch out later if need
-		'type' => 'tweet',
-		'offset' => '0',
-		'perpage' => '100',
-		'sort' => 'date',
-		'maxtime' => $string
+	$txtOffset = 0;
 
-		#'offset' => int(rand(50)) * 10,
-		
-
-	);
 
 	$url = 'http://otter.topsy.com/search.js?';
 
-	#$nurl = 'http://otter.topsy.com/search.js?q=%23tbt+"remember+when"\&type=tweet\&offset=0\&perpage=10\&maxtime=1391288415\&apikey=09C43A9B270A470B8EB8F2946A9369F3';
+	$runCount = 0;
+	while (scalar @goodlegs < 30){
+		$txtOffset += 100 * $runCount;
 
-	foreach (keys %params){
-		$url .= $_ . "=" . $params{$_} . '\&';
-	}
+		%params = (
+			'q' => '%23tbt+when',
+			'apikey' => '09C43A9B270A470B8EB8F2946A9369F3', # I don't know how long this one will work, but should be able to switch out later if need
+			'type' => 'tweet',
+			'offset' => '0',
+			'perpage' => '100',
+			'sort' => 'date',
+			'maxtime' => $string,
 
-	$result = `curl $url`;
-	$data = parse_json($result);
-
-	#print $result;
-	
-	foreach (@{$data->{response}->{list}}){
-		$twt = $_->{title};
-
+			'offset' => $txtOffset
 		
-		# Some filters:
 
-		$twt =~ s/#.+?(\s|$)/it /ig; # replace hashtags with "it"
-		$twt =~ s/http.+?(\s|$)//ig; # remove links
-		$twt =~ s/\@.+?(\s|$)/you /ig; # replace mentions with "you"
+		);
 
-		push(@tweets, $twt);
-		
-	}
 
-	print "Tweets: " . scalar(@tweets) . "\n";
-
-	if (scalar(@tweets) == 0){
-		print "Result: $result";
-	}
-
-	foreach (@tweets){
-		if (/when\s+?(.+?)([\?\!\.\;\,\:\&\-\"]|$)/ig){
-
-			my $leg = $1;
-			$leg =~ s/[^\w\s\d]//ig;
-
-			unless($usedText{"$leg"} eq 'y'){
-				push (@legs, $leg);
-			}		
-				
+		foreach (keys %params){
+			$url .= $_ . "=" . $params{$_} . '\&';
 		}
+
+		$result = `curl $url`;
+		$data = parse_json($result);
+
+		#print $result;
+		
+		foreach (@{$data->{response}->{list}}){
+			$twt = $_->{title};
+
+			
+			# Some filters:
+
+			$twt =~ s/#.+?(\s|$)/it /ig; # replace hashtags with "it"
+			$twt =~ s/http.+?(\s|$)//ig; # remove links
+			$twt =~ s/\@.+?(\s|$)/you /ig; # replace mentions with "you"
+
+			push(@tweets, $twt);
+			
+		}
+
+		print "Tweets: " . scalar(@tweets) . "\n";
+
+		if (scalar(@tweets) == 0){
+			print "Result: $result";
+		}
+
+		foreach (@tweets){
+			if (/when\s+?(.+?)([\?\!\.\;\,\:\&\-\"]|$)/ig){
+
+				my $leg = $1;
+				$leg =~ s/[^\w\s\d]//ig;
+
+				unless($usedText{"$leg"} eq 'y'){
+					push (@legs, $leg);
+				}		
+					
+			}
+		}
+
+		@goodlegs = uniq @legs;
 	}
-
-	@goodlegs = uniq @legs;
-
 	print "Legs: " . scalar(@goodlegs) . "\n";
 	return @goodlegs;
 }
